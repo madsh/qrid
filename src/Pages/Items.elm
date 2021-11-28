f1e52fc9cdc5d3d66391293860c3740f6f57e8da
Mads Hjorth <madsh@me.com>
2021-11-28 16:12:52 +0100

    errorhandling and optional fields

--- src/Pages/Items.elm
+++ src/Pages/Items.elm
@@ -4,12 +4,14 @@ import Auth
 import Html exposing (..)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode as D exposing (Decoder, at, field, int, map3, string)
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required, optional)
+
+
 
 
 import Html.Attributes exposing (class)
 import Html.Attributes as A exposing (id, type_)
-
 import Page
 import Request exposing (Request)
 import Shared
@@ -32,26 +34,22 @@ page shared _ =
 
 
 -- INIT
-
-
-type alias Model =
-    { items : ItemList
-    , errorMessage : String
-     }
-
-
 type alias Item = 
     { name : String
     , description : String
     , qrid : String  }
 
-type alias ItemList = 
-    List Item
+
+type alias Model =
+    { items : List Item
+    , errorMessage : Maybe String
+     }
+
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { items = [], errorMessage = ""}, Cmd.none )
+    ( { items = [], errorMessage = Nothing}, Cmd.none )
 
 
 
@@ -60,7 +58,7 @@ init =
 
 type Msg
     = SendHttpRequest 
-    | DataRecieved (Result Http.Error ItemList)
+    | DataRecieved (Result Http.Error (List Item))
 
 
 getItems : Cmd Msg
@@ -72,14 +70,14 @@ getItems =
 
 
 itemListDecoder : Decoder (List Item)
-itemListDecoder = D.list itemDecoder          
+itemListDecoder = Decode.list itemDecoder          
 
 itemDecoder : Decoder Item 
 itemDecoder = 
-    map3 Item
-        ( at ["name"] string)
-        ( at ["description"] string)
-        ( at ["qrid"] string)
+    Decode.succeed Item 
+        |> required "qrid" string
+        |> required "name" string
+        |> optional "description" string " - no description - "
 
 
 update : Storage -> Msg -> Model -> ( Model, Cmd Msg )
@@ -91,10 +89,28 @@ update storage msg model =
         DataRecieved (Ok itemList) ->            
             ( { model | items = itemList } , Cmd.none)
 
-        DataRecieved (Err error) -> 
-            ( {model | errorMessage = "Undhandled JSON error?"}, Cmd.none)
+        DataRecieved (Err httpError) -> 
+            ( {model | errorMessage = Just (buildErrorMessage httpError)}, Cmd.none)
 
+buildErrorMessage : Http.Error -> String
+buildErrorMessage httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
 
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "Unable to reach server."
+
+        Http.BadStatus statusCode ->
+            "Request failed with status code: " ++ String.fromInt statusCode
+
+        Http.BadBody message ->
+            message
+
+{- VIEW -}
 
 view : Auth.User -> Model -> View Msg
 view user model =
@@ -106,7 +122,7 @@ view user model =
             , Html.p [ class "font-lead"] [ Html.text "Here is what you have registered so far"]            
             , Html.div [][Html.text (" items indeholder " ++ (String.fromInt( List.length model.items)))]
             , viewForm user model
-            , viewTable user model            
+            , viewItemsOrError model            
             ]
          ]
         
@@ -121,22 +137,43 @@ viewForm user model =
     ]
 
 
-viewTable : Auth.User -> Model -> Html Msg
-viewTable user model = 
+viewItemsOrError : Model -> Html Msg 
+viewItemsOrError model = 
+    case model.errorMessage of 
+        Just message ->
+            viewError message
+        Nothing ->
+            viewItems model.items
+
+viewError : String -> Html Msg 
+viewError errorMessage = 
+    let 
+        errorHeading = "Couldn't fetch data at this time"
+    in
+    Html.div[class "alert alert-error"]
+    [
+        Html.div [class "alert-body"] 
+        [ Html.p [class "alert-heading"] [Html.text errorHeading ]
+        , Html.p [class "alert-text"] [Html.text ("Error: " ++ errorMessage)]
+        ]
+    ]
+
+
+viewItems : List Item -> Html Msg
+viewItems items = 
     Html.div[class "table--reponsive-scroll"]
-    [ Html.table [class "table"] 
-      [ Html.thead [][ viewTableHeader user model]
-      , Html.tbody [](List.map viewTableRow model.items)
+    [ Html.table [class "table table--compact"] 
+      [ Html.thead []
+        [ Html.tr[]
+          [ Html.th [][Html.text "Name"]
+          , Html.th [][Html.text "Description"]
+          , Html.th [][Html.text "QRID"]
+          ]
+        ]
+      , Html.tbody [](List.map viewTableRow items)
       ]
     ]
 
-viewTableHeader : Auth.User -> Model -> Html Msg
-viewTableHeader user model = 
-    Html.tr[]
-    [ Html.th [][Html.text "Name"]
-    , Html.th [][Html.text "Description"]
-    , Html.th [][Html.text "QRID"]
-    ]
 
 
 viewTableRow: Item -> Html Msg 
