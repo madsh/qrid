01e18fb386d763a783ae41d92d2c6053cea6e2b1
Mads Hjorth <madsh@me.com>
2021-11-29 20:55:49 +0100

    banner on scan

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,7 +1,7 @@
 module Pages.Home_ exposing (Model, Msg, page, view)
 
 import Auth
-import Html
+import Html exposing (Html)
 import Html.Events as Events
 import Html.Attributes as Attr
 import Page
@@ -9,6 +9,7 @@ import Request exposing (Request)
 import Shared
 import Storage exposing (Storage)
 import UI
+import Dict exposing (Dict)
 import View exposing (View)
 
 
@@ -60,8 +61,28 @@ view user request _ =
         UI.layout user request [
             Html.main_ [ Attr.class "container page-container", Attr.id "main-content"] 
             [ Html.h1 [ Attr.class ""] [ Html.text ("Hello, " ++ user.name ++ "!") ]
+            , fromScanner user request
             , Html.button [ Events.onClick ClickedSignOut ] [ Html.text "Sign out" ]
+            
             ]
          ]
         
     }
+
+
+fromScanner : Auth.User -> Request -> Html Msg
+fromScanner _  req = 
+    case Dict.get "qrid" req.query of 
+    Nothing ->
+        Html.div [][]
+    Just val ->
+        Html.div[ Attr.class "alert alert-success"]
+        [ Html.div [Attr.class "alert-body"]
+          [ Html.p [Attr.class "alert-heading"][ Html.text "You just scanned a qrid!"]
+          , Html.p [Attr.class "alert-text"]
+              [ Html.text "And i believe your item id is "
+              , Html.a [ Attr.href ("/item?"++val)][Html.text val]
+              ]
+          ]
+
+        ]
