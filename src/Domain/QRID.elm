0f52dcce4d9cf66772830e5cd96d4e9b7a633e3e
Mads Hjorth <madsh@me.com>
2021-12-02 18:29:42 +0100

    better new item

--- src/Domain/QRID.elm
+++ src/Domain/QRID.elm
@@ -1,8 +1,18 @@
-module Domain.QRID exposing (generate)
+module Domain.QRID exposing (generate, parse)
 
 
-import UUID exposing (UUID)
+import UUID exposing (UUID, Error)
+
 
 
 generate : UUID
 generate = UUID.forName "myap" UUID.dnsNamespace
+
+
+parse : String -> Result String String
+parse input = 
+    if (String.length input < 24 ) 
+        then Err "must be over 24"
+    else 
+        Ok input
+
